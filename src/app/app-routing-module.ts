4cf0549d39e6b5d0231e1fa7ed645a8ce07a2066
Hashim Sobhi <hashimsobhi97@gmail.com>
2025-06-08 23:27:09 +0300

    handled bootstrap dependency

--- src/app/app-routing-module.ts
+++ src/app/app-routing-module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule, Routes } from '@angular/router';
-import { NgbCollapseModule } from '@ng-bootstrap/ng-bootstrap';
 import { DashboardComponent } from './components/dashboard/dashboard';
 import { CustomerComponent } from './components/customer/customer';
 import { OrderComponent } from './components/order/order';
@@ -13,8 +12,8 @@ const routes: Routes = [
   { path: 'customers', component: CustomerComponent },
   { path: 'orders', component: OrderComponent },
   { path: 'products', component: ProductComponent },
-  { path: '', redirectTo: '/dashboard', pathMatch: 'full' } , 
-  { path: '**', redirectTo: '/dashboard'}  
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' } ,
+  { path: '**', redirectTo: '/dashboard'}
 
 ];
 
@@ -24,7 +23,6 @@ const routes: Routes = [
     RouterModule.forRoot(routes),
     BrowserModule,
     FormsModule,
-    NgbCollapseModule,
   ],
   exports: [RouterModule]
 })
